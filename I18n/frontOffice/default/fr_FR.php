f8294a621a50c80cb0ca88dac719889b0427e9f2
Soldras <apenalver@openstudio.fr>
2015-10-19 11:04:01 +0200

    add front translation

---
+++ I18n/frontOffice/default/fr_FR.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "Fail" => "Raté",
+    "Go Back" => "Retour",
+    "See you" => "Au revoir",
+    "Success" => "Gagné",
+    "You correctly anwser of questions" => "Vous avez répondu correctement à toutes les questions.",
+    "You have too mutch error try another time." => "Vous avez trop d'erreurs, essayez une autre fois.",
+    "You receivle shortlyy a message if you win !" => "Vous recevrez prochainement un mail si vous etes tiré au sort."
+);
